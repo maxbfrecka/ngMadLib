f3e01dbc0e2c23d5b6df773c5bb978583a037e62
maxbfrecka <maxbfrecka@gmail.com>
2016-11-07 17:00:40 -0500

    controller

---
+++ controller.js
@@ -0,0 +1,12 @@
+angular.module('myApp', [])
+    .controller('madlib', function($scope) {
+    	$scope.male_name;
+    	$scope.job_title;
+    	$scope.tedius_task;
+    	$scope.dirty_task;
+    	$scope.celebrity;
+    	$scope.useless_skill;
+    	$scope.obnoxious_celebrity;
+    	$scope.huge_number;
+    	$scope.adjective;
+    });
