eb58c7437816f7960ee887d091097cca59e64169
maxbfrecka <maxbfrecka@gmail.com>
2016-11-09 17:17:25 -0500

    all

--- controller.js
+++ controller.js
@@ -1,25 +1,26 @@
-var myApp= angular.module('myApp', ['ngMessages'])
+angular.module('myApp', ['ngMessages'])
+    .controller('MadlibController', function() {
+    	var _this = this;
+        this.reset=false;
 
-    .controller('madlib', function() {
-    	var ml = this;
-        ml.reset=false;
+        this.submit = function(){ _this.reset=!_this.reset; };
 
-        ml.submit = function(){
-            reset=!reset;
-                    };
+        this.resetVals = function(){
+            _this.reset=!_this.reset;
+            _this.maleName = '';
+            _this.jobTitle = '';
+            _this.tediousTask = '';
+            _this.dirtyTask = '';
+            _this.celebrity = '';
+            _this.adjective = '';
+            _this.obnoxiousCelebrity = '';
+            _this.hugeNumber = '';
+            _this.uselessSkill = '';
+        }
+
+        this.double = function(value){return value*2;};
 
 
-        ml.double = function(value){return value*2;};
-
-    	ml.male_name;
-    	ml.job_title;
-    	ml.tedius_task;
-    	ml.dirty_task;
-    	ml.celebrity;
-    	ml.useless_skill;
-    	ml.obnoxious_celebrity;
-    	ml.huge_number;
-    	ml.adjective;
     });
 
 
