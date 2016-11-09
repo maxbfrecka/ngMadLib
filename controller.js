954cffa0c963c04b53e37b793720b5f7c2e3cfa0
maxbfrecka <maxbfrecka@gmail.com>
2016-11-09 15:22:15 -0500

    all

--- controller.js
+++ controller.js
@@ -1,6 +1,16 @@
-angular.module('myApp', [])
+var myApp= angular.module('myApp', ['ngMessages'])
+
     .controller('madlib', function() {
     	var ml = this;
+        ml.reset=false;
+
+        ml.submit = function(){
+            reset=!reset;
+                    };
+
+
+        ml.double = function(value){return value*2;};
+
     	ml.male_name;
     	ml.job_title;
     	ml.tedius_task;
@@ -11,3 +21,8 @@ angular.module('myApp', [])
     	ml.huge_number;
     	ml.adjective;
     });
+
+
+    //how to make the function work?
+
+    //how to clear all of the inputs
